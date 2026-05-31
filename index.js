48036bfa13aec8e8f004314ba7c7d5ad6b5ace75
Nayan Pal <nayanpalmns@gmail.com>
2026-05-31 14:43:33 +0530

    updated the port

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ app.get('/download-db', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
 });
